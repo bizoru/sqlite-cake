a6bd2248c739b56a3e917498152781b581bbc6a3
Steven Sierra <ssierra@ngs.org>
2015-06-19 11:19:22 -0500

    Add setup.py to distribute.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='sqlite-cake',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='GPL v2.0',
+    author='Steven',
+    author_email='bizoru@gmail.com',
+    description='A simple SQLite Model Definition Helper.'
+)
